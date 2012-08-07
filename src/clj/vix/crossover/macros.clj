d06b3ecb9b1ef66df67c70898e496cef5f610e85
Filip de Waard <fmw@vix.io>
2012-08-07 15:46:11 +0200

    added crossover directory

---
+++ src/clj/vix/crossover/macros.clj
@@ -0,0 +1,37 @@
+;; cljs/macros/vix/macros.clj: Useful macros for ClojureScript code.
+;; Copyright 2011-2012, Vixu.com, F.M. (Filip) de Waard <fmw@vixu.com>.
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;; 
+;; http://www.apache.org/licenses/LICENSE-2.0
+;; 
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
+(ns vix.crossover.macros)
+
+(comment
+  (deftest test-routes
+    (is (= (routes "/admin/blog/edit"
+                   ["admin" "foo"] :unmatched
+                   ["admin" :feed "edit"] (identity (:feed params))
+                   ["admin" "bar"] :unmatched)
+           "blog"))
+
+    (is (= (routes "/admin/blog/edit/foo"
+                   ["admin" "foo"] :unmatched
+                   ["admin" :feed "edit"] (identity (:feed params))
+                   ["admin" "bar"] :unmatched
+                   :fallback :fallen-back)
+           :fallen-back))))
+
+(defmacro routes [uri-path & clauses]
+  (when-let [[route-segments body & clauses] clauses]
+    `(if-let [~'params (vix.core/route-matches ~route-segments ~uri-path)]
+      ~body
+      (routes ~uri-path ~@clauses))))
---
+++ src/clj/vix/crossover/shared.clj
@@ -0,0 +1,6 @@
+(ns vix.crossover.shared
+  (:require;*CLJSBUILD-REMOVE*;-macros
+    [vix.crossover.macros :as macros]))
+
+(defn make-example-text []
+  nil)
